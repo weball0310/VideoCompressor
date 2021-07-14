fd281b55ac4a984fa5ece7b865a26576f35d6304
litong <15097109000@163.com>
2021-07-14 14:37:06 +0800

    添加github 配置

--- app/src/main/java/com/vincent/videocompress/MainActivity.java
+++ app/src/main/java/com/vincent/videocompress/MainActivity.java
@@ -68,8 +68,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent();
                 /* 开启Pictures画面Type设定为image */
-                //intent.setType("video/*;image/*");
-                //intent.setType("audio/*"); //选择音频
+                intent.setType("video/*;image/*");
+                intent.setType("audio/*"); //选择音频
                 intent.setType("video/*"); //选择视频 （mp4 3gp 是android支持的视频格式）
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(intent, REQUEST_FOR_VIDEO_FILE);
@@ -84,11 +84,11 @@ public class MainActivity extends AppCompatActivity {
                 VideoCompress.compressVideoLow(tv_input.getText().toString(), destPath, new VideoCompress.CompressListener() {
                     @Override
                     public void onStart() {
-//                        tv_indicator.setText("Compressing..." + "\n"
-//                                + "Start at: " + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()));
-//                        pb_compress.setVisibility(View.VISIBLE);
-//                        startTime = System.currentTimeMillis();
-//                        Util.writeFile(MainActivity.this, "Start at: " + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()) + "\n");
+                        tv_indicator.setText("Compressing..." + "\n"
+                                + "Start at: " + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()));
+                        pb_compress.setVisibility(View.VISIBLE);
+                        startTime = System.currentTimeMillis();
+                        Util.writeFile(MainActivity.this, "Start at: " + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()) + "\n");
                     }
 
                     @Override
@@ -103,28 +103,28 @@ public class MainActivity extends AppCompatActivity {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-//                        String previous = tv_indicator.getText().toString();
-//                        tv_indicator.setText(previous + "\n"
-//                                + "Compress Success!" + "\n"
-//                                + "End at: " + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()));
-//                        pb_compress.setVisibility(View.INVISIBLE);
-//                        endTime = System.currentTimeMillis();
-//                        Util.writeFile(MainActivity.this, "End at: " + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()) + "\n");
-//                        Util.writeFile(MainActivity.this, "Total: " + ((endTime - startTime) / 1000) + "s" + "\n");
-//                        Util.writeFile(MainActivity.this);
+                        String previous = tv_indicator.getText().toString();
+                        tv_indicator.setText(previous + "\n"
+                                + "Compress Success!" + "\n"
+                                + "End at: " + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()));
+                        pb_compress.setVisibility(View.INVISIBLE);
+                        endTime = System.currentTimeMillis();
+                        Util.writeFile(MainActivity.this, "End at: " + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()) + "\n");
+                        Util.writeFile(MainActivity.this, "Total: " + ((endTime - startTime) / 1000) + "s" + "\n");
+                        Util.writeFile(MainActivity.this);
                     }
 
                     @Override
                     public void onFail() {
-//                        tv_indicator.setText("Compress Failed!");
-//                        pb_compress.setVisibility(View.INVISIBLE);
-//                        endTime = System.currentTimeMillis();
-//                        Util.writeFile(MainActivity.this, "Failed Compress!!!" + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()));
+                        tv_indicator.setText("Compress Failed!");
+                        pb_compress.setVisibility(View.INVISIBLE);
+                        endTime = System.currentTimeMillis();
+                        Util.writeFile(MainActivity.this, "Failed Compress!!!" + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()));
                     }
 
                     @Override
                     public void onProgress(float percent) {
-//                        tv_progress.setText(String.valueOf(percent) + "%");
+                        tv_progress.setText(String.valueOf(percent) + "%");
                     }
                 });
             }
@@ -144,8 +144,8 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_FOR_VIDEO_FILE && resultCode == RESULT_OK) {
             if (data != null && data.getData() != null) {
-//                inputPath = data.getData().getPath();
-//                tv_input.setText(inputPath);
+                inputPath = data.getData().getPath();
+                tv_input.setText(inputPath);
 
                 try {
                     inputPath = Util.getFilePath(this, data.getData());
@@ -164,8 +164,8 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
-//                inputPath = "/storage/emulated/0/DCIM/Camera/VID_20170522_172417.mp4"; // 图片文件路径
-//                tv_input.setText(inputPath);// /storage/emulated/0/DCIM/Camera/VID_20170522_172417.mp4
+                inputPath = "/storage/emulated/0/DCIM/Camera/VID_20170522_172417.mp4"; // 图片文件路径
+                tv_input.setText(inputPath);// /storage/emulated/0/DCIM/Camera/VID_20170522_172417.mp4
             }
         }
     }
